5265c7bc09410af5fad6034ecac1a1bbba256652
hiraguchisayaka <59869369+hiraguchisayaka@users.noreply.github.com>
2020-01-28 14:43:05 +0900

    asynchronous-update add

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
     config.i18n.default_locale = :ja
     g.stylesheets false
